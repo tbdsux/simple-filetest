d26d9a924d70c93989f8ac54d3fb6cf5d595b5bc
TheBoringDude <iamcoderx@gmail.com>
2021-06-15 12:16:17 +0800

    add functions

--- file.go
+++ file.go
@@ -3,38 +3,90 @@ package simplefiletest
 import (
 	"errors"
 	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"os"
+	"strings"
 )
+
+// file reader wrapper
+func fileReader(filename string) (string, error) {
+	if FileExists(filename) {
+		f, err := ioutil.ReadFile(filename)
+		if err != nil {
+			return "", err
+		}
+
+		return string(f), nil
+
+	}
+
+	return "", fmt.Errorf("%s does not exist", filename)
+}
 
 // SimilarContents attempts to open and compare the contents of `filename` to `compare`.
 // It returns true and nil if it is similar while false and an error otherwise.
 func SimilarContents(filename string, compare string) (bool, error) {
-	// filename should exist
-	if Exists(filename) {
-		f, err := ioutil.ReadFile(filename)
-		if err != nil {
-			return false, err
-		}
-
-		contents := string(f)
-
-		if contents == compare {
-			return true, nil
-		}
-
-		return false, errors.New("both strings are not similar")
+	contents, err := fileReader(filename)
+	if err != nil {
+		return false, err
 	}
 
-	return false, fmt.Errorf("%s does not exist", filename)
+	if contents == compare {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+// HasContents attempts to open and check the contents of `filename` if
+// it includes `compare`.
+func HasContents(filename string, compare string) (bool, error) {
+	contents, err := fileReader(filename)
+	if err != nil {
+		return false, err
+	}
+
+	if strings.Contains(contents, compare) {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+// details: https://stackoverflow.com/a/66405130
+func pathExists(path string) (fs.FileInfo, bool) {
+	info, err := os.Stat(path)
+
+	if errors.Is(err, os.ErrNotExist) {
+		return nil, false
+	}
+
+	return info, true
 }
 
 // Exists checks if `f` exists in the path or not.
-// Details: https://stackoverflow.com/a/66405130
 func Exists(f string) bool {
-	if _, err := os.Stat(f); errors.Is(err, os.ErrNotExist) {
+	_, ok := pathExists(f)
+	return ok
+}
+
+// FileExists checks if file exists in path.
+func FileExists(file string) bool {
+	info, ok := pathExists(file)
+	if !ok {
 		return false
 	}
 
-	return true
+	return !info.IsDir()
+}
+
+// DirExists checks if the dir exists in path.
+func DirExists(dir string) bool {
+	info, ok := pathExists(dir)
+	if !ok {
+		return false
+	}
+
+	return info.IsDir()
 }
--- file_test.go
+++ file_test.go
@@ -1,7 +1,6 @@
 package simplefiletest
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -16,11 +15,36 @@ func TestSimilarContents(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	fmt.Println(ok)
+	if !ok {
+		t.Fatal("file's content should be similar")
+	}
+}
+
+func TestHasContents(t *testing.T) {
+	ok, err := HasContents("go.mod", "module github.com/TheBoringDude/simple-filetest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !ok {
+		t.Fatal("file should contain the string")
+	}
 }
 
 func TestExists(t *testing.T) {
 	if !Exists("go.mod") {
 		t.Fatal("exists wrong return")
+	}
+}
+
+func TestFileExists(t *testing.T) {
+	if !FileExists("file.go") {
+		t.Fatal("file should exist")
+	}
+}
+
+func TestFolderExists(t *testing.T) {
+	if DirExists("non-existstent folder") {
+		t.Fatal("folder doesn't exist but function return that it exists")
 	}
 }
